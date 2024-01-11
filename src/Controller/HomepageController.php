d30a7a2d769fbd4a32767a43f92b7cfebfab4655
euriade <ljessicalejeune@gmail.com>
2024-01-11 14:40:11 +0100

    display all products and persist product in bdd

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findAll();
+
         return $this->render('base.html.twig', [
-            'controller_name' => 'HomepageController',
+            'products' => $products,
         ]);
     }
 }
